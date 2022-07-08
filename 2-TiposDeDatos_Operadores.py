08381abab88c727d5c81168a3eefa9d2f53f9f82
Carlos Ardon <ardon7528@gmail.com>
2022-07-08 12:13:09 -0600

    Primeras clases de curso de python

---
+++ 2-TiposDeDatos_Operadores.py
@@ -0,0 +1,37 @@
+#Tipos de Datos, variables y Operadores Aritmeticos.add()
+
+# variables 
+
+# Tipos de datos numericos
+numeroUno = 1
+print(numeroUno)
+
+numeroDecimal = 2.46
+print(numeroDecimal)
+
+#alguanas operaciones que podemos hacer
+#sumar el numero + 2
+numeroUno += 2
+print(numeroUno)
+
+#Potencias y el modulo
+potencia = 2**3
+modulo = 11%2 #Retorna el reciduo de la divicion realizada.
+print(potencia)
+print(modulo)
+
+#Tipos de datos de texto
+texto =  "Esta es una frase de texto"
+print(texto)
+
+#diferentes maneras de mostrar las comillas
+texto2 =  "Esta es 'una frase' de texto"
+texto3 =  'Esta es "una frase" de texto'
+
+#Texto con formato
+textoConFormato = """ Esto es un texto 
+con formato"""
+
+print(texto2)
+print(texto3)
+print(textoConFormato)
